7ed9037daa027cae41a1586a2b5a504fd64776b0
Parzival8971 <esh8971@gmail.com>
2022-09-14 01:35:30 +0900

    memoization을 위한 useMemo을 추가한다

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -24,7 +24,7 @@ const App = () => {
         id: dataId.current++,
       };
     });
-    console.log(initData);
+    // console.log(initData);
     setData(initData);
   };
 
@@ -44,7 +44,7 @@ const App = () => {
     };
     dataId.current += 1;
     setData([newItem, ...data]);
-    console.log(newItem);
+    // console.log(newItem);
   };
 
   const onRemove = (targetId) => {
@@ -61,9 +61,25 @@ const App = () => {
     );
   };
 
+  const getDiaryanalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryanalysis;
+  console.log(getDiaryanalysis);
+
   return (
     <div className='App'>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
